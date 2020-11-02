daeaf256214a6276a9be8d80880b1a94d1a21f49
Ross Hathaway <hathaway.ross@gmail.com>
2020-11-01 22:34:19 -0500

    start using template to generate blog posts

--- editable/make-blog.js
+++ editable/make-blog.js
@@ -2,9 +2,16 @@ const fs = require('fs')
 const path = require('path')
 const matter = require('gray-matter')
 const md = require('markdown-it')()
+const ejs = require('ejs')
+
+const templateFile = fs.readFileSync(path.join(__dirname, 'templates/blog.html'), 'utf-8')
+
+const template = ejs.compile(templateFile, {
+    cache: true,
+    filename: 'blog.html',
+});
 
 const blogFileNames = fs.readdirSync(path.join(__dirname, '/blog'))
-// for each file
 
 for (let fileName of blogFileNames) {
     const postMarkdown = fs.readFileSync(path.join(__dirname, 'blog', fileName), 'utf-8')
@@ -12,25 +19,6 @@ for (let fileName of blogFileNames) {
     const postHtml = md.render(frontmatterSplitPost.content)
 
     // pass both to ejs
+    const output = template({ ...frontmatterSplitPost.data, postHtml })
+    console.log(output)
 }
-
-// {
-//     content: 'Our first goal has almost been achieved! It is only by all of your heartfelt help that we have come this far. We are so close to the top of the mountain. But this last $9,000 is weighing heavily on our shoulders.\n' +
-//       '\n' +
-//       '**PLEASE HELP TO LIGHTEN OUR LOAD  \n' +
-//       ' IN THE LAST STRETCH UP THE MOUNTAIN!**\n' +
-//       '\n' +
-//       '**![](/media/sittingonmountain.jpg)**',
-//     data: {
-//       date: 2013-01-22T23:53:36.000Z,
-//       title: 'We Need Your Help!',
-//       subtitle: '',
-//       description: 'Our request to you...',
-//       author: 'HTN Staff'
-//     },
-//     isEmpty: false,
-//     excerpt: ''
-//   } <p>Our first goal has almost been achieved! It is only by all of your heartfelt help that we have come this far. We are so close to the top of the mountain. But this last $9,000 is weighing heavily on our shoulders.</p>
-//   <p><strong>PLEASE HELP TO LIGHTEN OUR LOAD<br>
-//   IN THE LAST STRETCH UP THE MOUNTAIN!</strong></p>
-//   <p><strong><img src="/media/sittingonmountain.jpg" alt=""></strong></p>
