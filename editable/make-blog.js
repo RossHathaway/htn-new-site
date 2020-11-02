453e94a2dac4210603fd6ca13afa1e767cc953f5
Ross Hathaway <hathaway.ross@gmail.com>
2020-11-01 21:49:40 -0500

    clean up

--- editable/make-blog.js
+++ editable/make-blog.js
@@ -1,15 +1,15 @@
 const fs = require('fs')
 const path = require('path')
 const matter = require('gray-matter')
+const md = require('markdown-it')()
 
 const blogFileNames = fs.readdirSync(path.join(__dirname, '/blog'))
-
 // for each file
+
 for (let fileName of blogFileNames) {
     const postMarkdown = fs.readFileSync(path.join(__dirname, 'blog', fileName), 'utf-8')
     const frontmatterSplitPost = matter(postMarkdown)
+    const postHtml = md.render(frontmatterSplitPost.content)
 
-    console.log(frontmatterSplitPost)
-    // pass that to markdown-it
     // pass both to ejs
 }
