49817961859b6a5acbf18aa7ae4468ba60b35314
Ross Hathaway <hathaway.ross@gmail.com>
2020-11-02 01:24:30 -0500

    add module to copy editable md into html template for events page

---
+++ editable/build-events.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const path = require('path')
+const md = require('markdown-it')()
+const ejs = require('ejs')
+
+const postMarkdown = fs.readFileSync(path.join(__dirname, 'events.md'), 'utf-8')
+
+const eventsInfoHtml = md.render(postMarkdown)
+
+ejs.renderFile(
+    path.join(__dirname, 'templates/events.html'),
+    { eventsInfoHtml },
+    { cache: true, fileName: 'events.html' },
+    (err, result) => {
+        if (err) {
+            throw new Error(err)
+        }
+        fs.writeFileSync('dist/misc/events.html', result)
+    }
+)
