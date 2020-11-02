66410597657ad4183c1622fc067b7410cca5278d
Ross Hathaway <hathaway.ross@gmail.com>
2020-11-01 20:39:31 -0500

    make-blog: read all blog files

--- editable/make-blog.js
+++ editable/make-blog.js
@@ -1,12 +1,15 @@
 const fs = require('fs')
 const path = require('path')
+const matter = require('gray-matter')
 
-// read editable/templates/blog
 const blogFileNames = fs.readdirSync(path.join(__dirname, '/blog'))
 
-console.log(blogFileNames)
-
 // for each file
-// pass to gray matter
-// pass that to markdown-it
-// pass both to ejs
+for (let fileName of blogFileNames) {
+    // read file sync
+    const postMarkdown = fs.readFileSync(path.join(__dirname, 'blog', fileName), 'utf-8')
+    console.log(postMarkdown)
+    // pass to gray matter
+    // pass that to markdown-it
+    // pass both to ejs
+}
