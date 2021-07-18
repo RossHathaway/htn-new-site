6923743f4be5db56f8a6e433c169f226e2093400
Ross Hathaway <hathaway.ross@gmail.com>
2021-07-18 12:31:36 -0400

    first attempt to replace /index.html

--- removeEndings.js
+++ removeEndings.js
@@ -1,5 +1,6 @@
 const rw = require('rw-stream')
 const readDir = require('recursive-readdir')
+const replaceStream = require('replacestream')
 
 const files = readDir('./dist', [ignoreFunc], readDirCb)
 
@@ -7,21 +8,13 @@ function ignoreFunc(file, stats) {
     return !file.endsWith('.html') && !stats.isDirectory()
 }
 
-function readDirCb(err, result) {
+async function readDirCb(err, result) {
     if (err) {
         throw new Error(err)
     }
 
     for (const file of result) {
-        console.log('file', file)
-
-        // const {fd, readStream, writeStream} = await rw(path, options);
-
-        // create read stream from file
-        // make sure chunk does not end with any of these chars: /index.htm
-        // if so, add to chunk
-        // if not, check for existence of /index.html and replace with ''
-        // check for existence of .html and replace with ''
-        // write chunk to file
+        const { fd, readStream, writeStream } = await rw(file, {})
+        readStream.pipe(replaceStream('/index.html', '')).pipe(writeStream)
     }
 }
