1a554f15569488abc3ea86828c325177dc4dd0c6
Ross Hathaway <hathaway.ross@gmail.com>
2021-08-16 23:45:55 -0400

    reload on passthrough file change

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/js')
 
     return {
+        passthroughFileCopy: true,
         dir: {
             input: 'src',
             output: 'dist',
