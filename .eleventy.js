ace58a639fef56a5b39a26a7e3abb88d2040b369
Ross Hathaway <hathaway.ross@gmail.com>
2021-08-29 00:29:19 -0400

    not working attempt to show 404 when running browser sync

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/css')
     eleventyConfig.addPassthroughCopy('src/js')
@@ -5,6 +6,25 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/webfonts')
     eleventyConfig.addPassthroughCopy('src/media')
     eleventyConfig.addPassthroughCopy('src/js')
+
+    // 404 not working
+    // eleventyConfig.setBrowserSyncConfig({
+    //     callbacks: {
+    //         ready: function (err, bs) {
+    //             bs.addMiddleware('*', (req, res) => {
+    //                 const content_404 = fs.readFileSync('dist/404.html')
+    //                 console.log('404 -------', content_404)
+    //                 // Add 404 http status code in request header.
+    //                 res.writeHead(404, {
+    //                     'Content-Type': 'text/html; charset=UTF-8',
+    //                 })
+    //                 // Provides the 404 content without redirect.
+    //                 res.write(content_404)
+    //                 res.end()
+    //             })
+    //         },
+    //     },
+    // })
 
     return {
         passthroughFileCopy: true,
