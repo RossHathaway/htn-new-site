ed5853463be0dc478fac1e397b605c36b2abad9b
Ross Hathaway <hathaway.ross@gmail.com>
2021-09-19 21:17:30 -0400

    use working optimized image from 11ty image plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,57 @@
 const fs = require('fs')
+const read = require('recursive-readdir')
+const path = require('path')
+const Image = require('@11ty/eleventy-img')
+
+function ignoreFunc(file, stats) {
+    return stats.isDirectory()
+}
+
+async function imageShortcode(src, alt, sizes) {
+    let metadata = await Image(src, {
+        widths: [1000, 500, 300, null],
+        // widths,
+        filenameFormat: function (id, src, width, format) {
+            const extension = path.extname(src)
+            const name = path.basename(src, extension)
+
+            return `${name}-${width}w.${format}`
+        },
+        outputDir: 'dist/media',
+        urlPath: '/media/',
+    })
+
+    let imageAttributes = {
+        alt,
+        sizes,
+        loading: 'lazy',
+        decoding: 'async',
+    }
+
+    return Image.generateHTML(metadata, imageAttributes, {
+        whitespaceMode: 'inline',
+    })
+}
+
+// read('/media', [], ignoreFunc, async function (err, files) {
+//     if (err) {
+//         throw err
+//     }
+//     for (const file of files) {
+//     }
+// })
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/css')
     eleventyConfig.addPassthroughCopy('src/js')
     eleventyConfig.addPassthroughCopy('src/slick')
     eleventyConfig.addPassthroughCopy('src/webfonts')
-    eleventyConfig.addPassthroughCopy('src/media')
+    // eleventyConfig.addPassthroughCopy('src/media')
     eleventyConfig.addPassthroughCopy('src/js')
+
+    eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode)
+    eleventyConfig.addLiquidShortcode('image', imageShortcode)
+    eleventyConfig.addJavaScriptFunction('image', imageShortcode)
 
     eleventyConfig.addFilter('dateFilter', (date) =>
         new Date(date).toDateString().slice(4)
